e7d45fa895aa22e291d4070f40b1e99a49a0896d
mario-aj <mariojorge1997@mail.ru>
2025-11-30 17:51:51 -0300

    Implement initial state management utility with create function and useStore hook

---
+++ src/lib/index.ts
@@ -0,0 +1,38 @@
+import { useSyncExternalStore } from "react";
+
+interface SetState<T> {
+  (newState: Partial<T> | ((state: T) => Partial<T>)): void;
+}
+
+export function create<T>(initFunction: (set: SetState<T>, get: () => T) => T) {
+  let state: T = {} as T;
+  const listeners = new Set<VoidFunction>();
+
+  const setState: SetState<T> = (newState) => {
+    if (typeof newState === "function") {
+      state = { ...state, ...newState(state) };
+    } else {
+      state = { ...state, ...newState };
+    }
+    listeners.forEach((listener) => listener());
+  };
+
+  const getState = () => state;
+
+  state = initFunction(setState, getState);
+
+  const useStore = () => {
+    return useSyncExternalStore(
+      (callback) => {
+        listeners.add(callback);
+
+        return () => {
+          listeners.delete(callback);
+        };
+      },
+      () => getState()
+    );
+  };
+
+  return useStore;
+}
